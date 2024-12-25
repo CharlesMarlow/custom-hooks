2234aee1b4e75e4e120d8dea9c566393663eb91b
CharlesMarlow <gordon.nadav@gmail.com>
2024-12-25 10:50:28 +0100

    add responsive design, colours & css improvements

--- src/App.tsx
+++ src/App.tsx
@@ -16,14 +16,23 @@ const App = () => {
   );
   const [searchTerm, setSearchTerm] = useState<string>('');
   const handleSearch = (value: string) => console.log(`Searching ${value}`);
-  const [debouncedValue] = useDebounce(searchTerm, handleSearch, 300);
+  const [debouncedValue] = useDebounce(searchTerm, handleSearch, 200);
+
+  const bgColors = [
+    'bg-blue-100',
+    'bg-green-100',
+    'bg-yellow-100',
+    'bg-zinc-200',
+    'bg-purple-100',
+  ];
 
   return (
-    <div className='h-screen grid grid-rows-2 grid-cols-3 gap-4 p-4'>
+    <div className='h-screen grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 p-4'>
       {/* Top Sections */}
       <Section
         title='useCounter'
-        description='A simple counter hook to demonstrate state and actions.'
+        description='A counter hook to demonstrate state and actions.'
+        className={bgColors[0]}
       >
         <div className='flex items-center justify-between space-x-4 mt-20'>
           <button
@@ -41,25 +50,32 @@ const App = () => {
           </button>
         </div>
       </Section>
+
       <Section
         title='useMousePosition'
         description="Tracks the mouse's x and y positions in real-time."
+        className={bgColors[1]}
       >
         <div className='flex items-center justify-between space-x-4 mt-20'>
-          <div className='border border-zinc-400 p-2'>{`X Position: ${xPosition}`}</div>
-          <div className='border border-zinc-400 p-2'>{`Y Position: ${yPosition}`}</div>
+          <div className='border border-zinc-400 rounded p-2'>{`X Position: ${xPosition}`}</div>
+          <div className='border border-zinc-400 rounded p-2'>{`Y Position: ${yPosition}`}</div>
         </div>
       </Section>
+
       <Section
         title='useCounterTracker'
         description='Tracks a counter and records the timestamp of the last increment.'
+        className={bgColors[2]}
       >
-        <div className='flex flex-col items-center space-x-4 mt-20'>
-          <button
-            onClick={incrementCountAndSetLastClick}
-          >{`Count is: ${state.count}`}</button>
+        <div className='flex flex-col items-center space-x-4 space-y-6 mt-20'>
+          <p onClick={incrementCountAndSetLastClick}>
+            <span className='font-semibold'>Count is: </span>
+            <button className='px-4 py-2 bg-zinc-500 text-white rounded hover:bg-zinc-600 transition'>
+              {state.count}
+            </button>
+          </p>
           <p>
-            Last clicked at:{' '}
+            <span className='font-semibold mt-10'>Last clicked at: </span>
             {state.lastClick
               ? new Date(state.lastClick).toLocaleString()
               : 'Never'}
@@ -71,22 +87,28 @@ const App = () => {
       <Section
         title='useFetchData'
         description='Fetches data from an API, managing loading state and errors.'
+        className={`${bgColors[3]} col-span-1 sm:col-span-2`}
       >
-        <div className='flex flex-col items-center space-x-4 mt-6'>
+        <div className='flex flex-col items-center space-x-4 mt-12'>
           {loading && <div>Loading...</div>}
           {error && <div>{error}</div>}
           {data && data.length > 0 && (
             <div>
               {data.slice(0, 5).map((post) => (
-                <p key={post.id}>{`Title: ${post.title}`}</p>
+                <p key={post.id}>
+                  <span className='font-semibold'>Title: </span>
+                  {post.title}
+                </p>
               ))}
             </div>
           )}
         </div>
       </Section>
+
       <Section
         title='useDebounce'
         description='Delays updating a value until after a specified wait time and invokes a callback with the debounced value.'
+        className={bgColors[4]}
       >
         <div className='flex flex-col items-center space-x-4 mt-16'>
           <input
@@ -94,7 +116,9 @@ const App = () => {
             value={searchTerm}
             onChange={(e) => setSearchTerm(e.target.value)}
           />
-          <p>Search term: {debouncedValue}</p>
+          <p>
+            <span className='font-semibold'>Search term:</span> {debouncedValue}
+          </p>
         </div>
       </Section>
     </div>
--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -4,13 +4,21 @@ interface SectionProps {
   title: string;
   children: ReactNode;
   description?: string;
+  className?: string;
 }
 
-const Section: FC<SectionProps> = ({ title, children, description }) => {
+const Section: FC<SectionProps> = ({
+  title,
+  children,
+  description,
+  className,
+}) => {
   return (
-    <div className='p-6 border rounded-lg shadow-md bg-white'>
+    <div
+      className={`p-6 border rounded-lg shadow-md ${className || ''}`}
+    >
       <h2 className='text-2xl font-semibold mb-2 underline'>{title}</h2>
-      {description && <p className='text-gray-600 mb-4'>{description}</p>}
+      {description && <p className='text-gray-700 mb-4'>{description}</p>}
       <div>{children}</div>
     </div>
   );
